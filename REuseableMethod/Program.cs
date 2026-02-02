17f8cd5c0d5d23d8253cf21863bdcf0294f5d138
Ashton-Tolman <Tolman.ash22@gmail.com>
2026-02-02 08:09:51 -0700

    Add project files.

---
+++ REuseableMethod/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace REuseableMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //pause
+            Console.Read();
+        }
+    }
+}
