a19e850d52f181282da2415ea96b4b02e72a919f
Ashton-Tolman <Tolman.ash22@gmail.com>
2026-02-02 08:56:12 -0700

    Cool array example with header

--- REuseableMethod/Program.cs
+++ REuseableMethod/Program.cs
@@ -1,11 +1,52 @@
-﻿namespace REuseableMethod
+﻿using System.Security.Cryptography;
+
+namespace REuseableMethod
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            TestRandomNumber();
             //pause
             Console.Read();
+        }
+
+        static void TestRandomNumber()
+        {
+            int max = 10, min = 5, currentNumber = 0, paddingLeft = 2, paddingRight = paddingLeft += 1;
+            
+            int[] results = new int[12];
+            
+            for (int i = 0; i < 1000; i++)
+            {
+                //Console.Write(RandomNumber(10, 5).ToString().PadLeft(2));
+                currentNumber = RandomNumber(max,min);
+                results[currentNumber] += 1;
+            }
+
+            //Make column headers
+            for (int i = 0; i < results.Length; i++)
+            {
+                Console.Write(i.ToString().PadLeft(paddingLeft).PadRight(paddingRight) + "|");
+            }
+            Console.WriteLine();
+            
+            //Get array data
+            foreach (int result in results)
+            {
+                Console.Write(result.ToString().PadLeft(paddingLeft).PadRight(paddingRight) +"|");
+            }
+        }
+        static int RandomNumber(int max, int min)
+        {
+
+           
+            
+            Random rand = new Random();
+            int range = ++max - min; //get the actual number of random values possible
+            int randomNumber = rand.Next(range);
+            randomNumber += min;
+            return randomNumber;
         }
     }
 }
